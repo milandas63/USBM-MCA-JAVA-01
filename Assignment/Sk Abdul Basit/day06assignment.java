7a8ed0f88aa3cdfadd8a8db296cac094de7af9b7
Sk Abdul Basit <146845688+SkAbdulBasit@users.noreply.github.com>
2023-10-09 20:51:28 +0530

    Add files via upload
    Rectangularframe

---
+++ Assignment/Sk Abdul Basit/day06assignment.java
@@ -0,0 +1,33 @@
+
+    import java.util.*;
+    import java.util.List;
+    public class day06assignment {
+        public static void printInFrame(List<String> strings) {
+            int maxLength = 0;
+            for (String str : strings) {
+                if (str.length() > maxLength) {
+                    maxLength = str.length();
+                }
+            }
+            int frameWidth = maxLength + 4;
+
+            for (int i = 0; i < frameWidth; i++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+            for (String str : strings) {
+                System.out.println("* " + str + " ".repeat(maxLength - str.length()) + " *");
+            }
+
+            for (int i = 0; i < frameWidth; i++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        public static void main(String[] args) {
+            List<String> wordList = List.of("Hello", "World", "in", "a", "frame");
+            printInFrame(wordList);
+        }
+    }
+

81674a610a71ed3405798d557a41c0521d676212
PravasiniDash123 <146924867+PravasiniDash123@users.noreply.github.com>
2023-10-11 19:07:04 +0530

    Add files via upload

---
+++ Assignment/Pravasini Dash/Rectanglestar.java
@@ -0,0 +1,28 @@
+package Java;
+
+public class Rectanglestar { 
+	public static void main(String[]args) 
+	{ 
+		int width=2; 
+		for(int i=0;i<=1;i++) 
+		{ 
+			for(int j=0;j<width;j++) 
+			{ 
+				System.out.println("********"); 
+				{ 
+					System.out.print("* Hello * \n"); 
+					System.out.print("* World * \n"); 
+					System.out.print("* in * \n"); 
+					System.out.print("* a * \n"); 
+					System.out.print("* frame * \n");
+
+
+
+
+				}
+		}
+	} 
+		System.out.println("*********");
+
+} 
+}
